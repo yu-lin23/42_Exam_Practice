4a6bf688b2a96bd35307e8e184bf3b4ca8d07420
Sherry <50695343+yu-lin42@users.noreply.github.com>
2019-08-28 16:43:18 +0200

    Fixed

--- level05/cycle_detector/cycle_detector.c
+++ level05/cycle_detector/cycle_detector.c
@@ -6,19 +6,19 @@
 /*   By: yu-lin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/28 08:15:46 by yu-lin            #+#    #+#             */
-/*   Updated: 2019/08/28 10:23:23 by yu-lin           ###   ########.fr       */
+/*   Updated: 2019/08/28 15:41:02 by yu-lin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "list.h"
 
-int     cycle_detector(const t_list *lst)
+int     cycle_detector(const t_list *list)
 {
     t_list      *slow;
     t_list      *fast;
 
-    slow = (t_list *)lst;
-    fast = (t_list *)lst->next;
+    slow = (t_list *)list;
+    fast = (t_list *)list->next;
     while (fast && fast->next)
     {
         if (fast == slow)
